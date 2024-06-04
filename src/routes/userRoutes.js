6b62a725e9e88f5a8616b6cac62c887ccad8f66a
iviruu <ivan.kok.ru@gmail.com>
2024-06-04 08:29:34 +0200

    feat: Actualizar permisos de autenticación en las rutas de usuario
    Se actualizaron los permisos de autenticación en las rutas de usuario para restringir el acceso a ciertas funcionalidades. Esto incluye cambios en las rutas '/create', '/teacher/create', '/teacher/alumnos/:teacher_id', '/teacher/alumnosList', '/alumno/relacion/:alumno_id', '/alumno/update_relacion/:relacion_id', '/profe/relacion/:teacher_id', y '/profe/delete_relacion/:relacion_id'.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,18 +14,18 @@ router.get('/', authenticateToken([2,1]), getUser);
 router.get('/saque', authenticateToken([2,1]), getSaques);
 router.get('/resultados', authenticateToken([2,1]), getResultados);
 router.get('/resultados/saque/:id', authenticateToken([2,1]), getResultadosByIdSaque);
-router.post('/create', authenticateToken([2,1]), createResultados);
+router.post('/create', authenticateToken([2]), createResultados);
 router.get('/resultados/alumno/:id', authenticateToken([2,1]), getResultadosByIdAlumno);
 router.post('/resultados/solo', authenticateToken([2,1]), postResultadosSaqueAlumno);
-router.post('/teacher/create', authenticateToken([2,1]), createRelacionAlumnoProfesor);
-router.get('/teacher/alumnos/:teacher_id', authenticateToken([2,1]), getAlumnosByProfesor);
-router.get('/teacher/alumnosList', authenticateToken([2,1]), getAlumnosList);
-router.get('/alumno/relacion/:alumno_id', authenticateToken([2,1]), getRelacion);
-router.post('/alumno/update_relacion/:relacion_id', authenticateToken([2,1]), updateRelacion);
+router.post('/teacher/create', authenticateToken([2]), createRelacionAlumnoProfesor);
+router.get('/teacher/alumnos/:teacher_id', authenticateToken([2]), getAlumnosByProfesor);
+router.get('/teacher/alumnosList', authenticateToken([2]), getAlumnosList);
+router.get('/alumno/relacion/:alumno_id', authenticateToken([1]), getRelacion);
+router.post('/alumno/update_relacion/:relacion_id', authenticateToken([1]), updateRelacion);
 router.post('/updateUser', authenticateToken([2,1]), updateUser);
 router.post('/uploadPhoto', authenticateToken([2,1]), uploadFileMiddleware, uploadPhoto);
-router.get('/profe/relacion/:teacher_id', authenticateToken([2,1]), getRelacionIdProfesor);
-router.delete('/profe/delete_relacion/:relacion_id', authenticateToken([2,1]), deleteRelacion);
+router.get('/profe/relacion/:teacher_id', authenticateToken([2]), getRelacionIdProfesor);
+router.delete('/profe/delete_relacion/:relacion_id', authenticateToken([2]), deleteRelacion);
 
 
 export default router;
