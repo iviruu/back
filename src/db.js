51e50e78d5da22172bb38d502041e986b07bb7fb
iviruu <ivan.kok.ru@gmail.com>
2024-09-20 13:09:14 +0200

    pruebas render 1

--- src/db.js
+++ src/db.js
@@ -2,18 +2,13 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 require('dotenv').config();
-dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER_NAME,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST_NAME || 'localhost',
-    port: process.env.PORT || 3306,
-    dialect: 'mysql'
-  }
-);
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  dialect: 'mysql',
+});
 
 const syncroModel = async () => {
   try {
